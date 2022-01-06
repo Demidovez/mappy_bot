4dd0fc14a5a056fa4062d6b938ac10e698b55775
Demidovez <nikolaydemidovez@gmail.com>
2022-01-06 12:49:35 +0300

    bot response on mappy problem

---
+++ index.js
@@ -0,0 +1,34 @@
+import { Telegraf } from "telegraf";
+import dotenv from "dotenv";
+import express from "express";
+
+dotenv.config();
+
+const bot = new Telegraf(process.env.BOT_TOKEN); //сюда помещается токен, который дал botFather
+
+const app = express();
+const host = "0.0.0.0";
+const port = process.env.PORT || 8081;
+
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+app.post("/add_problem", (req, res) => {
+  res.status(200).type("text/plain");
+  res.send("OK");
+
+  const problem = req.body;
+
+  bot.telegram.sendMessage(
+    process.env.ADMIN_TELEGRAM_ID,
+    `Новая проблема!\n\nОт: ${problem.user}\nПроблема: ${problem.problem}`
+  );
+
+  console.log(problem);
+});
+
+bot.launch();
+
+app.listen(port, host, function () {
+  console.log(`Bot listens http://${host}:${port} :: ${new Date()}`);
+});
